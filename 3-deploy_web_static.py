d2bf1e29bff83dbbebf18232fec1fa31ba16c096
612teres <612teres@gmail.com>
2024-06-12 02:27:43 +0300

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
-import os
+from fabric.context_managers import cd, hide,\
+        settings, show, path, prefix, lcd, quiet, warn_only,\
+        remote_tunnel, shell_env
+from fabric.decorators import hosts, roles,\
+        runs_once, with_settings, task, serial, parallel
+from fabric.operations import require, prompt,\
+        put, get, run, sudo, local, reboot, open_shell
+from fabric.state import env, output
+from fabric.utils import abort, warn, puts, fastprint
+from fabric.tasks import execute
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
-
+import os
 
 env.hosts = ["34.229.254.78", "100.26.159.154"]
 env.user = 'ubuntu'
